d7c38680a3bc6d49078e2e9d605219ee3ff6b63c
Dominic Sabatier <dsabatier@gmail.com>
2019-11-27 18:08:46 -0700

    Update GameEventBaseEditor.cs

--- Noodlepop/Game Events/Editor/GameEventBaseEditor.cs
+++ Noodlepop/Game Events/Editor/GameEventBaseEditor.cs
@@ -6,8 +6,6 @@ namespace Noodlepop.GameEvents
     [CustomEditor(typeof(GameEventBase), true)]
     public class GameEventBaseEditor : Editor
     {
-        [SerializeField] private AudioSource _previewer;
-        
         public override void OnInspectorGUI()
         {
             EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
