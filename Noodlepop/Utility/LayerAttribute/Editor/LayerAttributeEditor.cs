6be03cb7e7b16b34adaf90e85e0ba963db4ab33b
Dominic <dsabatier@gmail.com>
2019-11-30 22:37:53 -0700

    Added Layer and Tag attributes

---
+++ Noodlepop/Utility/LayerAttribute/Editor/LayerAttributeEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Noodlepop
+{
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    class LayerAttributeEditor : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.intValue = EditorGUI.LayerField(position, label,  property.intValue);
+        }
+    }
+}
